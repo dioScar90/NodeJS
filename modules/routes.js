de35ff6fc8342fbe882e9c4d723e24255ec5575d
Diogo L Scarmagnani <diogols@live.com>
2022-09-21 14:23:16 -0300

    vai

--- modules/routes.js
+++ modules/routes.js
@@ -1,16 +1,16 @@
-const { Router } = require('express');
-const UserController = require('./constrollers/UserController');
+// const { Router } = require('express');
+// const UserController = require('./constrollers/UserController');
 
-const routes = Router();
+// const routes = Router();
 
-routes.get('/health', (req, res) => {
-    return res.status(200).json({
-        message: 'Server is on...'
-    });
-});
+// routes.get('/health', (req, res) => {
+//     return res.status(200).json({
+//         message: 'Server is on...'
+//     });
+// });
 
-routes.get('/users', UserController.index);
-routes.get('/users/:id', UserController.show);
-routes.post('/users', UserController.store);
+// routes.get('/users', UserController.index);
+// routes.get('/users/:id', UserController.show);
+// routes.post('/users', UserController.store);
 
-module.exports = routes;
+// module.exports = routes;
