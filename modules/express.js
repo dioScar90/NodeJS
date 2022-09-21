fbe8345dbf82799fdfa5a176c4031e288a0f51da
Diogo L Scarmagnani <diogols@live.com>
2022-09-21 08:08:42 -0300

    express.static()

--- modules/express.js
+++ modules/express.js
@@ -4,6 +4,7 @@ const UserModel = require("../src/models/user.model");
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 app.set("views", "src/views");
